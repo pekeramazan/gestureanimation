dad07e54ba158fbf57322bbb5936664e690aee08
pekeramazan <pekeramazan@gmail.com>
2020-05-26 06:41:30 +0300

    test

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,13 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+   
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -34,84 +22,118 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState ();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyHomePageState extends State<MyHomePage>  with SingleTickerProviderStateMixin{
+  int tabsCount = 0;
+  int doubleTabsCount = 0;
+  int longPressCount = 0;
+  double xPosition=0.0;
+  double yPosition=0.0;
+  double boxSize=0.0;
+  double fullBoxSize=150.0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+AnimationController controller;
+Animation<double> animation;
+
+ @override
+void initState() { 
+  super.initState();
+  controller=AnimationController(
+    duration:const Duration(milliseconds: 10000),
+    vsync: this
+  );
+  animation=CurvedAnimation(parent: controller,curve: Curves.bounceInOut);
+  animation.addListener(() {
+    setState((){
+      boxSize=fullBoxSize*animation.value;
+
     });
-  }
+    centerBox(context);
+  });
+  controller.forward();
+  
+}
+void dispose(){
+  controller.dispose();
+  super.dispose();
+}
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    //  if(xPosition==0.0)
+    // {
+    //   centerBox(context);
+    // }
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Gestures"),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            tabsCount++;
+          });
+        },
+        onDoubleTap: (){
+          setState(() {
+            doubleTabsCount++;
+          });
+        },
+        onLongPress:(){
+          setState(() {
+            longPressCount++;
+          });
+        },
+        onVerticalDragUpdate: (DragUpdateDetails value){
+          setState(() {
+            double delta =value.delta.dy;
+            yPosition +=delta;
+          });
+        },
+          onHorizontalDragUpdate: (DragUpdateDetails value){
+          setState(() {
+            double delta =value.delta.dx;
+            xPosition +=delta;
+          });
+        },
+        
+        
+        child: Stack(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Positioned(
+              left: xPosition,
+              top: yPosition,
+              child: Container(
+                width:  boxSize,
+                height: boxSize,
+                decoration: BoxDecoration(color: Colors.deepPurpleAccent),
+              ),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: Material(
+          child: Padding(
+        padding: EdgeInsets.only(left: 50.0, bottom: 10.0),
+        child: Text(
+            "Tap : $xPosition,Double Tap: $yPosition,Long Press: $longPressCount"),
+      )),
     );
+  }
+  void centerBox(BuildContext context){
+    xPosition=MediaQuery.of(context).size.width/2.0-boxSize/2.0;//konum alma
+    yPosition=MediaQuery.of(context).size.height/2.0-boxSize/2.0-30.0;
+    setState(() {
+      this.xPosition=xPosition;
+      this.yPosition=yPosition;
+    });
+
+    
+
   }
 }
